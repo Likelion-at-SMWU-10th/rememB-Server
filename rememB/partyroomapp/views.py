d0e6536f6d3f95d2ea570bab08c14e08f39fb771
happine2s <happine2s@sookmyung.ac.kr>
2022-08-16 23:40:25 +0900

    [fix] conflict error edit

--- rememB/partyroomapp/views.py
+++ rememB/partyroomapp/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from userapp.models import User
 from letterapp.models import Letter
 from rest_framework.views import APIView
@@ -22,10 +21,4 @@ class UserLetterView(APIView):
             serializer=LetterSumSerializer(user_letters, many=True)
             return Response(serializer.data)
         return Response({"error":"User Perimtion Denied"},status=status.HTTP_400_BAD_REQUEST)
-        
-=======
-from django.shortcuts import render
 
-from userapp.models import User
-
->>>>>>> develop
